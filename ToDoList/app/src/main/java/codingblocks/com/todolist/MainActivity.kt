e0666a7279343aec1d6b86695ff9b1e69a117406
Karandeep Singh <greatprincekarandeep@gmail.com>
2019-06-17 00:46:12 +0530

    Adding updated To-do list app

---
+++ ToDoList/app/src/main/java/codingblocks/com/todolist/MainActivity.kt
@@ -0,0 +1,45 @@
+package codingblocks.com.todolist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var list:ArrayList<String>
+    lateinit var adapt:ArrayAdapter<String>
+//    lateinit var adapt2:ArrayAdapter<String>
+//    var year= arrayOf("2010","2011","2012","2013","2014","2015","2016","2017","2018","2019")
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener(this)
+//        spinner.onItemSelectedListener
+        list= ArrayList()
+        adapt = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, list)
+//        adapt2=ArrayAdapter(this,android.R.layout.simple_spinner_item,year)
+//        adapt2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+//        getValues()
+
+    }
+//    fun getValues(){
+//        Toast.makeText(this,"spinner1"+spinner.selectedItem.toString(),Toast.LENGTH_LONG).show()
+//    }
+    override fun onClick(v: View?) {
+        if(v?.id==R.id.button){
+            list.add(et.text.toString())//+spinner.selectedItem.toString())
+            lv.adapter = adapt
+//            spinner.adapter=adapt2
+//            adapt.notifyDataSetChanged()
+            et.setText("")
+
+        }
+    }
+}
