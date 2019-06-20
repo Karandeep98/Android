8fc158eeb929b70a44ad6c84eab0cc9d20c91f3a
Karandeep Singh <greatprincekarandeep@gmail.com>
2019-06-21 00:55:57 +0530

    added spinner in to-do list app

--- ToDoList2/app/src/main/java/codingblocks/com/todolist2/MainActivity.kt
+++ ToDoList2/app/src/main/java/codingblocks/com/todolist2/MainActivity.kt
@@ -3,41 +3,39 @@ package codingblocks.com.todolist2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    lateinit var list:ArrayList<String>
-    lateinit var adapt: ArrayAdapter<String>
-//    lateinit var adapt2:ArrayAdapter<String>
-//    var year= arrayOf("2010","2011","2012","2013","2014","2015","2016","2017","2018","2019")
-
+class MainActivity : AppCompatActivity() {
+    lateinit var list: ArrayList<String>
+//    lateinit var adapt: ArrayAdapter<String>
+    //    lateinit var adapt2:ArrayAdapter<String>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener(this)
-//        spinner.onItemSelectedListener
-        list= ArrayList()
-        adapt = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, list)
-//        adapt2=ArrayAdapter(this,android.R.layout.simple_spinner_item,year)
-//        adapt2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val year = arrayOf("2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019")
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, year)
+        list = ArrayList()
+//        var spintext=""
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                Toast.makeText(applicationContext, "Please select an option", Toast.LENGTH_LONG).show()
+            }
 
-//        getValues()
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, id: Long) {
+                tv.text = year[position]
+            }
 
-    }
-    //    fun getValues(){
-//        Toast.makeText(this,"spinner1"+spinner.selectedItem.toString(),Toast.LENGTH_LONG).show()
-//    }
-    override fun onClick(v: View?) {
-        if(v?.id==R.id.button){
-            list.add(et.text.toString())//+spinner.selectedItem.toString())
-            lv.adapter = adapt
-//            spinner.adapter=adapt2
-//            adapt.notifyDataSetChanged()
+        }
+        button.setOnClickListener {
+            lv.adapter = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, list)
+
+            list.add(et.text.toString()+"("+tv.text.toString()+")")
             et.setText("")
-
         }
     }
 }
